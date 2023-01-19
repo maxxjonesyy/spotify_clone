0803e82e9446b2eff31319c9146bdae6d209740c
Maxwell Jones <maxxjonesyy@gmail.com>
2023-01-19 14:48:13 +1100

    loading icon abstracted to reusable component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
+
+function Loading() {
+  return <ClipLoader color='white' speedMultiplier='0.75' />;
+}
+
+export default Loading;
