2979889f4b0e3af9353e65e614b0c8536300b22f
Maxwell Jones <maxxjonesyy@gmail.com>
2023-01-19 14:50:01 +1100

    alter api scopes

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,9 @@ function App() {
   const CLIENT_ID = "dd12b92a35b24500b82ba2bcf25b7774";
   const REDIRECT_URI = "http://127.0.0.1:5173/";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const SCOPES = [
-    "user-library-read",
-    "user-read-email",
-    "user-read-private",
-    "user-read-playback-position",
-    "user-top-read",
-    "user-read-recently-played",
-    "user-follow-read",
-    "playlist-read-private",
-    "playlist-read-collaborative",
-    "app-remote-control",
-    "streaming",
-    "user-read-playback-state",
-    "user-modify-playback-state",
-    "user-read-currently-playing",
-  ];
+  const SCOPES = `user-read-recently-played user-library-read user-read-email user-read-private user-read-playback-position user-top-read user-follow-read playlist-read-private playlist-read-collaborative app-remote-control streaming user-read-playback-state user-modify-playback-state user-read-currently-playing`;
   const RESPONSE_TYPE = "token";
-  const AUTH_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scopes=${SCOPES}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+  const AUTH_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scope=${SCOPES}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
 
   const [token, setToken] = useState("");
   const [user, setUser] = useState("");
