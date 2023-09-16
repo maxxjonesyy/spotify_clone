e09e63c3b05f4f62ffc2d68806db84eed7657327
Maxwell Jones <maxxjonesyy@gmail.com>
2023-09-16 16:32:49 +1000

    Improve login screen UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,14 +2,27 @@ import React from "react";
 
 function Login({ AUTH_URL }) {
   return (
-    <div className='h-screen w-screen flex flex-col justify-center items-center gap-10'>
+    <div className='h-screen w-screen flex flex-col justify-center items-center gap-10 bg-spotifyGrey'>
       <img src='./spotify-icon.svg' alt='spotify logo' className='w-[150px]' />
-      <button
-        onClick={() => (window.location = AUTH_URL)}
-        className='h-[40px] w-[250px] text-white font-semibold bg-spotifyGreen rounded-full '
-      >
-        Login to Spotify
-      </button>
+
+      <div className='text-center'>
+        <h1 className='text-white font-bold text-3xl'>Spotify Clone</h1>
+      </div>
+
+      <div className='flex flex-col gap-5'>
+        <button
+          onClick={() => (window.location = AUTH_URL)}
+          className='h-[40px] w-[250px] text-white font-semibold bg-spotifyGreen rounded-full'
+        >
+          Login to Spotify
+        </button>
+
+        <button className=' h-[40px] w-[250px] text-white font-semibold bg-spotifyGreen rounded-full'>
+          <a href='https://www.spotify.com/au/signup' target='_blank'>
+            Sign Up
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
